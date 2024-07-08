9e5f787c49bd5e3b8901e181028c8adb36895e94
Arpit <arpit.vaghela@outlook.com>
2024-07-08 16:28:01 -0400

    replacing video with the image for better user experience

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import { Video } from "@/components/custom/Video";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -8,8 +8,14 @@ export default function RootLayout({
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 w-full h-screen">
       <div>{children}</div>
-      <div className="auth-assets hidden md:block">
-        <Video />
+      <div className="auth-assets hidden md:block relative">
+        <Image
+          src="/bg.jpg"
+          fill
+          quality={100}
+          alt="Background"
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
       </div>
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import withPlaiceholder from "@plaiceholder/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -8,4 +9,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPlaiceholder(nextConfig);
