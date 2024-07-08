849017c74b110f77d6666e64c05d0d8b34090647
Arpit <arpit.vaghela@outlook.com>
2024-07-08 16:33:41 -0400

    remove plaiceholder modules

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import withPlaiceholder from "@plaiceholder/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -9,4 +8,4 @@ const nextConfig = {
   },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
